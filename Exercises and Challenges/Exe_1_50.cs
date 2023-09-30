36c52aec7d76f61d21dbe85fc8bea63877b4731e
Eterna Forren <eternaforren.dev@gmail.com>
2023-09-30 14:20:00 +0400

    Created repository. Added Project Euler folder and it's first completed exercise

---
+++ Exercises and Challenges/Exe_1_50.cs
@@ -0,0 +1,35 @@
+﻿using global::System;
+using global::System.Collections.Generic;
+using global::System.IO;
+using global::System.Linq;
+using global::System.Net.Http;
+using global::System.Threading;
+using global::System.Threading.Tasks;
+
+namespace ProjectEulerExercises
+{ 
+    class Exe_1_50
+    {
+        static void Main()
+        {
+            #region 1. Multiples of 3 and 5
+            // Find the sum of all the multiples of 3 and 5 below 1000.
+
+            int sum = 0;
+
+            for (int i = 3; i < 1000; i++)           // [3, 999]
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+            #endregion
+
+        }
+    }
+}
